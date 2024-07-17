b7a7fc89c41df7adb7aa65a12bfb29bba9cbb770
Samrod Shenassa <samrod@synctree.com>
2024-07-17 02:33:12 -0700

    Set session expiration back to 2 hours.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -24,7 +24,7 @@ export const CLIENT_STATE_DISPLAYS = (clientLink, clientName) => ({
 
 export const DEFAULT_PRESET_NAME = "Basic Settings";
 export const LINK_PLACEHOLDER = "custom link";
-export const EXPIRE_SESSION_SECONDS = 30;
+export const EXPIRE_SESSION_SECONDS = 7200;
 
 export const DB_LINKS = "links";
 export const DB_GUIDES = "guides";
